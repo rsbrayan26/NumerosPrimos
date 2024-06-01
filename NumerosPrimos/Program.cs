05cab79fffc569e138dd6ea41beeb1faf1766265
Yhors Brayan <rs.brayan26@gmail.com>
2024-06-01 15:04:08 -0500

    Agregar archivos de proyecto.

---
+++ NumerosPrimos/Program.cs
@@ -0,0 +1,34 @@
+﻿List<int> nPrimos = new List<int>() { 2, 3 };
+Boolean ban;
+int num = 2,count;
+
+for (int i = 0; i < 100; i++)
+{
+    ban = true;
+    while (ban)
+    {
+        count = 0;
+        foreach (var item in nPrimos)
+        {
+            if (num % item == 0)
+            {
+                break;
+            }
+            count++;
+        }
+        if (count == nPrimos.Count)
+        {
+            nPrimos.Add(num);
+            ban= false;
+            break;
+        }
+        num++;
+    }
+}
+
+for (int i = 0; i < nPrimos.Count; i++)
+{
+    Console.WriteLine($"Primo #{i+1}\t{nPrimos[i]}");
+}
+
+Console.ReadLine();
